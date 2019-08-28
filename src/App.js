542c10918ffd8e4c08839db05c40618b703f2503
McKay Waters <mckaypaswaters@gmail.com>
2019-08-28 15:15:59 -0600

    website design and data component added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DataComp from './DataComp'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="background">
+        <header className='blue-bar'>
+          <h4 className='home-logo'>Home</h4>
+        </header>
+          <div className='white-box-parent'>
+            <div className="data-white-box">
+              <DataComp />
+            </div>
+          </div>
+          <div className="buttons-parent">
+            <button className="previous-button">{'< Previous'}</button>
+            <button className='next-button'>{'Next >'}</button>
+          </div>
+      </div>
     </div>
   );
 }
